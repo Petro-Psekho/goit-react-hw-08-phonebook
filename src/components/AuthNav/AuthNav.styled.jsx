78231cafd918ee8ea5cd452c782a611ebc7dccfc
Petro Psekho <psekho.pp@gmail.com>
2023-04-15 19:19:07 +0300

    update

--- src/components/AuthNav/AuthNav.styled.jsx
+++ src/components/AuthNav/AuthNav.styled.jsx
@@ -6,7 +6,7 @@ export const Link = styled(NavLink)`
   text-decoration: none;
   padding: 12px;
   font-weight: 700;
-  color: #2a363b;
+  color: #1976d2;
 
   &.active {
     color: lightCoral;
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,16 +1,5 @@
-// import { Helmet } from 'react-helmet';
-// import { RegisterForm } from 'components/RegisterForm/RegisterForm';
-
 import SignUp from 'components/SignUp/SignUp';
 
 export default function Register() {
   return <SignUp />;
-  // return (
-  //   <div>
-  //     <Helmet>
-  //       <title>Registration</title>
-  //     </Helmet>
-  //     <RegisterForm />
-  //   </div>
-  // );
 }
