023249004c83fda89bee2533a4d6f6444e6ba69b
Petro Psekho <psekho.pp@gmail.com>
2023-04-15 16:56:03 +0300

    add material input

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,6 +7,8 @@ import { useContacts } from 'hooks';
 // import FormDialog from 'components/ModalFormDialog/MoadalFormDialog';
 
 import Button from '@mui/material/Button';
+// import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 // import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,8 +18,8 @@ import { addContact } from 'redux/contacts/operations';
 import {
   ErrMessage,
   FormWrap,
-  FormInput,
-  InputName,
+  // FormInput,
+  // InputName,
   SubmitBtn,
 } from 'components/ContactForm/ContactForm.styled';
 
@@ -56,6 +58,22 @@ export const ContactForm = ({ handleClose }) => {
 
   return (
     <FormWrap onSubmit={handleSubmit(formSubmitData)}>
+      <TextField
+        id="standard-basic"
+        label="Enter Name"
+        variant="standard"
+        autoComplete="off"
+        {...register('name', {
+          required: 'name is a required field',
+          minLength: { value: 3, message: 'min lenght is 3' },
+          pattern: {
+            value: /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+            message:
+              'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore dArtagnan.',
+          },
+        })}
+      />
+      {/* 
       <InputName>Name</InputName>
       <FormInput
         autoComplete="off"
@@ -69,10 +87,26 @@ export const ContactForm = ({ handleClose }) => {
           },
         })}
         placeholder="Enter Name"
-      />
+      /> */}
 
       <ErrMessage>{errors.name?.message}</ErrMessage>
 
+      <TextField
+        id="standard-basic"
+        label="Enter Namber"
+        variant="standard"
+        autoComplete="off"
+        {...register('number', {
+          required: 'number is a required field',
+          pattern: {
+            value:
+              /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+            message:
+              'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +.',
+          },
+        })}
+      />
+      {/* 
       <InputName>Number</InputName>
       <FormInput
         autoComplete="off"
@@ -86,7 +120,8 @@ export const ContactForm = ({ handleClose }) => {
           },
         })}
         placeholder="Enter Number"
-      />
+      /> */}
+
       <ErrMessage>{errors.number?.message}</ErrMessage>
 
       {errors.exampleRequired && <p>This field is required</p>}
